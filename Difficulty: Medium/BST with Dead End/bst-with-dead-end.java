3ec553e3c21279a77f832151b7c5e1fa3a8d619e
Navyasree17-J <jagarlamudi.navyasree@gmail.com>
2025-06-09 18:35:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/BST with Dead End/bst-with-dead-end.java
@@ -0,0 +1,34 @@
+/*
+class Node {
+        int data;
+        Node left, right;
+
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }
+}*/
+
+class Solution {
+    public boolean isDeadEnd(Node root) {
+        return hasDeadEnd(root, 1, Integer.MAX_VALUE);
+    }
+    
+    private boolean hasDeadEnd(Node node, int min, int max) {
+        if (min == max) return true;
+        
+        if (node.left != null && node.right != null) {
+            return hasDeadEnd(node.left, min, node.data - 1) || hasDeadEnd(node.right, node.data + 1, max);
+        }
+        
+        if (node.left != null) {
+            return hasDeadEnd(node.left, min, node.data - 1);
+        }
+        
+        if (node.right != null) {
+            return hasDeadEnd(node.right, node.data + 1, max);
+        }
+        
+        return false;
+    }
+}
