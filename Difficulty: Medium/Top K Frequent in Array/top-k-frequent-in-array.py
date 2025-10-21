07ca6bbf039b40836c2ecaff1162f84c6923c606
Navyasree17-J <jagarlamudi.navyasree@gmail.com>
2025-10-21 10:29:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top K Frequent in Array/top-k-frequent-in-array.py
@@ -0,0 +1,8 @@
+class Solution:
+	def topKFreq(self, arr, k):
+	    freq=Counter(arr)
+        sorted_items = sorted(freq.items(), key=lambda x: (x[1], x[0]), reverse=True)
+        return [num for num , _ in sorted_items[:k]]
+		# Code here
+		
+		
