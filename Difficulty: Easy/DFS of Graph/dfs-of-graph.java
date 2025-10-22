1f7284c76e6a458198e2d882db377844ebeec159
Navyasree17-J <jagarlamudi.navyasree@gmail.com>
2025-10-22 10:21:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.java
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -1,57 +1,25 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-            int V = Integer.parseInt(br.readLine().trim());
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-            for (int i = 0; i < V; i++) {
-                String[] input = br.readLine().trim().split(" ");
-                ArrayList<Integer> node = new ArrayList<>();
-                for (String s : input) {
-                    if (!s.isEmpty()) {
-                        node.add(Integer.parseInt(s));
-                    }
-                }
-                adj.add(node);
-            }
-
-            Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.dfs(adj);
-            for (int num : ans) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     // Function to return a list containing the DFS traversal of the graph.
     public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        boolean[] visited = new boolean[adj.size()];
-        dfsHelper(0, adj, visited, result);
+        // Code here
+         int V = adj.size(); // Total number of nodes
+        boolean[] visited = new boolean[V]; // Visited array to avoid revisits
+        ArrayList<Integer> result = new ArrayList<>(); // To store DFS traversal
+        
+        // Start DFS from node 0
+        dfsUtil(0, adj, visited, result);
         return result;
     }
-    private void dfsHelper(int node, ArrayList<ArrayList<Integer>> adj, boolean[] visited, ArrayList<Integer> result){
-        visited[node] = true;
-        result.add(node);
-        for(int i=0; i<adj.get(node).size();i++){
-            int neighbor = adj.get(node).get(i);
-            if(!visited[neighbor]){
-                dfsHelper(neighbor, adj, visited, result);
+
+    // Recursive helper function for DFS
+    private void dfsUtil(int node, ArrayList<ArrayList<Integer>> adj, boolean[] visited, ArrayList<Integer> result) {
+        visited[node] = true;       // Mark current node as visited
+        result.add(node);          // Add to DFS result
+        
+        // Visit all unvisited neighbors
+        for (int neighbor : adj.get(node)) {
+            if (!visited[neighbor]) {
+                dfsUtil(neighbor, adj, visited, result);
             }
         }
     }
