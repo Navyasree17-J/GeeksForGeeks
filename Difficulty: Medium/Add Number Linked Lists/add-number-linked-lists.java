370d509a5e9bf88082cf35b3732254d24d36e0b8
Navyasree17-J <jagarlamudi.navyasree@gmail.com>
2025-12-30 09:44:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.java
@@ -0,0 +1,55 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public Node addTwoLists(Node head1, Node head2) {
+        // code here
+         Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        // Push digits of first list
+        while (head1 != null) {
+            s1.push(head1.data);
+            head1 = head1.next;
+        }
+
+        // Push digits of second list
+        while (head2 != null) {
+            s2.push(head2.data);
+            head2 = head2.next;
+        }
+
+        int carry = 0;
+        Node result = null;
+
+        // Add digits
+        while (!s1.isEmpty() || !s2.isEmpty() || carry != 0) {
+            int sum = carry;
+
+            if (!s1.isEmpty()) sum += s1.pop();
+            if (!s2.isEmpty()) sum += s2.pop();
+
+            carry = sum / 10;
+
+            Node newNode = new Node(sum % 10);
+            newNode.next = result;
+            result = newNode;
+        }
+
+        // Remove leading zeros
+        while (result != null && result.data == 0 && result.next != null) {
+            result = result.next;
+        }
+
+        return result;
+    }
+}
