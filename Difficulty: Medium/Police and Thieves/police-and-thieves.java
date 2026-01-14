144f6fb6bf159eaa797a8a44325494ff4f729b46
Navyasree17-J <jagarlamudi.navyasree@gmail.com>
2026-01-14 15:24:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Police and Thieves/police-and-thieves.java
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.java
@@ -1,20 +1,33 @@
 class Solution {
     public int catchThieves(char[] arr, int k) {
         // code here
-         Queue<Integer> police=new LinkedList<>(),thieves=new LinkedList<>();
-        int caught=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]=='P') police.offer(i);
-            else thieves.offer(i);
+        Stack<Integer> st1=new Stack<>();
+        Stack<Integer> st2=new Stack<>();
+        int f=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]=='P')
+            st1.push(i);
+            else
+            st2.push(i);
+            
         }
-        while(!police.isEmpty() && !thieves.isEmpty()){
-            if(Math.abs(police.peek()-thieves.peek())<=k){
-                caught++;
-                police.poll();
-                thieves.poll();
-            }else if(police.peek()<thieves.peek()) police.poll();
-            else thieves.poll();
+        while(!st1.isEmpty()&&!st2.isEmpty())
+        {
+            if(Math.abs(st1.peek()-st2.peek())<=k)
+            {
+                st1.pop();
+                st2.pop();
+                f++;
+            }
+            else if(st2.peek()<st1.peek())
+            {
+                st1.pop();
+            }
+            else
+            st2.pop();
+            
         }
-        return caught;
+        return f;
     }
 }
