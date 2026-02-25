e1e40d356e9025f5065964d6fd81301f51f7d52b
Navyasree17-J <jagarlamudi.navyasree@gmail.com>
2026-02-25 09:44:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.java
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.java
@@ -1,71 +1,18 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
-
-// User function Template for Java
 class Solution {
-    static int longestSubarray(int[] arr, int k) {
+    public int longestSubarray(int[] arr, int k) {
         // Code Here
-        int n = arr.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int prefix[] = new int[n];
-        int ans = 0;
-        
-        for(int i=0; i<n ; i++) {
-            int prev = i>0 ? prefix[i-1] : 0;
+        int n = arr.length, count = 0, length = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for(int i = 0; i < n; i++){
+            if(arr[i] <= k) count--;
+            else count++;
             
-            prefix[i] += prev;
-            prefix[i] += arr[i] > k ? 1 : -1;
+            if(count > 0) length = Math.max(length, i + 1);
+            if(map.containsKey(count - 1)) length = Math.max(length, i - map.get(count - 1));
             
-            int curSum = prefix[i];
-            if(curSum>0) ans = Math.max(ans, i+1);
-            int required = curSum-1;
-            if(map.containsKey(required)) {
-                int startInd = map.get(required);
-                ans = Math.max(ans, i-startInd);
-            }
-            
-            if(!map.containsKey(prefix[i])) {
-                map.put(prefix[i], i);
-            }
-            
+            if(!map.containsKey(count)) map.put(count, i);
         }
-        
-        
-        return ans;
+        return length;
     }
 }
-
-
-//{ Driver Code Starts.
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-            int n = tokens.length;
-            int[] arr = new int[n];
-
-            int i = 0;
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                arr[i] = Integer.parseInt(token);
-                i++;
-            }
-
-            int k = Integer.parseInt(br.readLine().trim());
-            System.out.println(new Solution().longestSubarray(arr, k));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
