99c3440c1dd858dc887d392da836b61e54bbfe70
Navyasree17-J <jagarlamudi.navyasree@gmail.com>
2026-03-06 20:15:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.java
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.java
@@ -1,45 +1,42 @@
 class Solution {
-    public static String smallestWindow(String s, String p) {
+    public static String minWindow(String s, String p) {
         // code here
-        int n=s.length();
-        int m=p.length();
-        Map<Character,Integer>mp=new HashMap<>();
-        for(int i=0;i<m;i++)
-        {
-            char c=p.charAt(i);
-            mp.put(c,mp.getOrDefault(c,0)+1);
-        }
-        int cnt=mp.size();
-        int i=0;
-        int j=0;
-        int len=Integer.MAX_VALUE;
-        int st=-1;
-        while(j<n)
-        {
-            char c=s.charAt(j);
-            if(mp.containsKey(c))
-            {
-                mp.put(c,mp.get(c)-1);
-                if(mp.get(c)==0)cnt--;
-                while(cnt==0)
-                {
-                    if(len>j-i+1)
-                    {
-                        len=j-i+1;
-                        st=i;
-                    }
-                    char ch=s.charAt(i);
-                    if(mp.containsKey(ch))
-                    {
-                        mp.put(ch,mp.get(ch)+1);
-                        if(mp.get(ch)==1)cnt++;
-                    }
-                    i++;
-                }
+         int[]cnt=new int[128];
+
+    int req=p.length(),left=-1,min=s.length()+1;
+
+    for(char c:p.toCharArray()){
+
+        ++cnt[c];
+
+    }
+
+    for(int l=0,r=0;r<s.length();r++){
+
+        if(--cnt[s.charAt(r)]>=0)
+
+        --req;
+
+        while(req==0){
+
+            if(r-l+1<min){
+
+                left=l;
+
+                min=r-l+1;
+
             }
-            j++;
+
+            if(++cnt[s.charAt(l++)]>0)
+
+            ++req;
+
         }
-        if(st==-1)return "";
-        return s.substring(st,st+len);
+
+    }
+
+    
+
+      return left==-1?"":s.substring(left,left+min);
     }
 }
