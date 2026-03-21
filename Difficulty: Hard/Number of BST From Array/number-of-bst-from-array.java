1acab67ab381b34e7153ca5a4772230233c825fa
Navyasree17-J <jagarlamudi.navyasree@gmail.com>
2026-03-21 19:40:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Number of BST From Array/number-of-bst-from-array.java
+++ Difficulty: Hard/Number of BST From Array/number-of-bst-from-array.java
@@ -1,36 +1,70 @@
 class Solution {
     public ArrayList<Integer> countBSTs(int[] arr) {
         // Code here
-         int n = arr.length;
-        int[] arr2 = Arrays.copyOfRange(arr,0,n);
-        Arrays.sort(arr2);
-        
-        ArrayList<Integer> list = new ArrayList<>();
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        
-        for(int i = 0; i < n; i++){
-            hm.put(arr2[i],i);
-        }
-        
-        for(int i = 0; i < n; i++){
-            int ans = find(arr2,hm.get(arr[i]),0,n-1);
-            list.add(ans);
-        }
-        
-        return list;
+         int n=arr.length;
+
+    long[]cat=getCatlan(n);
+
+    int[]sortArray=arr.clone();
+
+    Arrays.sort(sortArray);
+
+    ArrayList<Integer>ans=new ArrayList<>();
+
+    for(int e:arr){
+
+        int leftCnt=findFirstOcc(sortArray,e);
+
+        int rightCnt=n-1-leftCnt;
+
+        ans.add((int)(cat[leftCnt]*cat[rightCnt]));
+
     }
-    private static int find(int[] arr, int idx, int si, int ei){
-        if(si == ei) return 1;
-        
-        int leftSum = 0, rightSum = 0;
-        
-        for(int i = idx-1; i >= si; i--) leftSum += find(arr,i,si,idx-1);
-        
-        for(int i = idx+1; i <= ei; i++) rightSum += find(arr,i,idx+1,ei);
-        
-        if(leftSum == 0) leftSum = 1;
-        if(rightSum == 0) rightSum = 1;
-        
-        return leftSum*rightSum;
+
+     return ans;  
+
+    }
+
+    private long[]getCatlan(int n){
+
+        long[]c=new long[n+1];
+
+        c[0]=1;
+
+        if(n>0) c[1]=1;
+
+        for(int i=2;i<=n;i++)
+
+        for(int j=0;j<i;j++){
+
+            c[i]+=(c[j]*c[i-1-j]);
+
+        }
+
+        return c;
+
+    }
+
+    private int findFirstOcc(int[]arr,int target){
+
+        int low=0,high=arr.length-1;
+
+        while(low<high){
+
+            int mid=low+(high-low)/2;
+
+            if(arr[mid]<target)
+
+                //ans=mid;
+
+                low=mid+1;
+
+            else
+
+            high=mid;
+
+        }
+
+        return low;
     }
 }
