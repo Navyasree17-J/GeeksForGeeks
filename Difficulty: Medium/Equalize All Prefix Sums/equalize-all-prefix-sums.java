05ef76d04550c28fe338498ead56227f16d9f26a
Navyasree17-J <jagarlamudi.navyasree@gmail.com>
2026-06-19 09:59:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Equalize All Prefix Sums/equalize-all-prefix-sums.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ArrayList<Integer> optimalArray(int[] arr) {
+        // code here
+        ArrayList<Integer>ans=new ArrayList<>();
+
+        ans.add(0);
+
+        for(int i=1;i<arr.length;i++){
+
+            int next=ans.get(i-1)+(arr[i]-arr[i/2]);
+
+            ans.add(next);
+
+        }
+
+        return ans;
+    }
+}
